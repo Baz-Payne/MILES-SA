07d462b6450afce565984cbedf19cc01da6d3d88
Baz-Payne <64762844+Baz-Payne@users.noreply.github.com>
2025-08-25 19:26:29 +0930

    Fixed demand trace

--- Multi Nodal Model/Data Creation/Demand/demand_trace_creation.py
+++ Multi Nodal Model/Data Creation/Demand/demand_trace_creation.py
@@ -1,16 +1,12 @@
 '''
-Important notes about the demand files
+Important note about the demand files
     - Demand files are given in half hourly amount in MW which are exclusive of hydrogen demand
-    - Hydrogen demand is given as a yearly total in TWh
-    - The annual hydrogen demand is taken from AEMO electricity and gas website
-    - This script splits the hydrogen demand equally and applies it to each half hour period of a given financial year
 '''
 
 
 
 import pandas as pd
 from datetime import datetime
-import calendar
 
 # Convert half hours to 24 hour EST
 half_hours = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48']
